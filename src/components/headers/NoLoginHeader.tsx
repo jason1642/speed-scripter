d245c1363d4fbca21077674c7840745df33ba3f4
jason1642 <jason1642@gmail.com>
2024-01-23 13:07:31 -0500

    Tailwind header

--- src/components/headers/NoLoginHeader.tsx
+++ src/components/headers/NoLoginHeader.tsx
@@ -3,32 +3,40 @@ import Link from 'next/link';
 import * as React from 'react';
 
 
+const linkStyles = 'px-4'
 
 interface INoLoginHeaderProps {
 }
 
 const NoLoginHeader: React.FunctionComponent<INoLoginHeaderProps> = () => {
   return (
-    <div className='flex-1 text-5xl'>
-        This is the header
+    <div className='flex width-full justify-between text-gray-100 text-3xl p-3 bg-blue-600'>
+    <h1 className='inline-block'>      
+            Speed Scripter
+        </h1>
 
 
+        <div className='inline-block'>
 
-        <Link 
+     <Link 
+     className={`${linkStyles}`}
             href={'/practice'}
         >
             Practice
         </Link>
-        <Link 
+        <Link className={`${linkStyles}`}
             href={'/challenge'}
         >
             Challenge
         </Link>
         <Link 
+        className={`${linkStyles}`}
             href={'/'}
         >
             Home
         </Link>
+        </div>
+    
     </div>
   );
 };
