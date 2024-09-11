8c582e5ec1fe8464a5b99108c28f2c3504700d1d
jason1642 <jason1642@gmail.com>
2024-09-11 13:25:56 -0400

    create user action

--- express/models/user.js
+++ express/models/user.js
@@ -1,4 +1,38 @@
 import mongoose from 'mongoose'
 
 
-// const 
+const userSchema = new mongoose.Schema({
+    _id: {
+        type: mongoose.Schema.Types.ObjectId
+    },
+    username: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 24
+      },
+    email: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 32,
+        unique: true, 
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 5, 
+        maxlength: 255
+      },
+      bio: {
+        type: String,
+        minlength: 0,
+        maxlength: 300,
+        default: ''
+      },
+
+}, {timestamps: true})
+
+const userModel = mongoose.model('User', userSchema)
+
+export default userModel
--- express/routes/users.js
+++ express/routes/users.js
@@ -1,4 +1,9 @@
 import express from 'express'
+import userModel from '../models/user.js'
+import _ from 'lodash'
+import mongoose from 'mongoose'
+
+
 let router = express.Router();
 
 /* GET users listing. */
@@ -6,4 +11,30 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+
+const createUser = async (req,res)=>{
+
+  let user = await userModel.findOne({$or: [
+    { username: req.body.username },
+    {email: req.body.email} 
+  ]})
+  if (user) return res.send('That username already exists')
+
+
+  user = new userModel(_.assign(_.pick(req.body, ['username', 'email', 'password']),
+  {
+    _id: new mongoose.Types.ObjectId() 
+  }
+))
+
+
+
+
+  console.log(user)
+  return res.send('returning post action')
+}
+
+router.post('/', createUser)
+
 export default router
+  
